067ecf3a59b548c96330f464ed9a3950f0fbece4
Sine Titan <565022817@qq.com>
2020-10-07 02:03:24 +0800

    三子棋游戏逻辑

---
+++ Week 01/TicTacToe/src/game.js
@@ -0,0 +1,120 @@
+export class GamePattern {
+    constructor() {
+        this.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+    }
+
+    getColor(x, y) {
+        return this.board[y][x];
+    }
+
+    setColor(x, y, color) {
+        this.board[y][x] = color;
+    }
+
+    clone() {
+        let res = Object.create(GamePattern.prototype);
+        res.board = JSON.parse(JSON.stringify(this.board));
+        return res;
+    }
+
+    check(color) {
+        for (let i = 0; i < 3; ++i) {
+            let win = true;
+            for (let j = 0; j < 3; ++j) {
+                if (this.board[i][j] !== color) {
+                    win = false;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        for (let j = 0; j < 3; ++j) {
+            let win = true;
+            for (let i = 0; i < 3; ++i) {
+                if (this.board[i][j] !== color) {
+                    win = false;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        {
+            let win = true;
+            for (let j = 0; j < 3; ++j) {
+                if (this.board[j][j] !== color) {
+                    win = false;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        {
+            let win = true;
+            for (let j = 0; j < 3; ++j) {
+                if (this.board[j][2 - j] !== color) {
+                    win = false;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    willWin(color) {
+        for (let i = 0; i < 3; ++i) {
+            for (let j = 0; j < 3; ++j) {
+                if (this.board[j][i]) {
+                    continue;
+                }
+
+                let tmp = this.clone();
+                tmp.setColor(i, j, color);
+                if (tmp.check(color)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+export const MoveResult = {
+    MOVE: 0, WIN: 1, CANTMOVE: 2
+}
+
+export class GameControl {
+    constructor() {
+        this.pattern = new GamePattern;
+        this.color = 1;
+        this.haveWin = false;
+    }
+
+    move(x, y) {
+        if (this.haveWin) {
+            return MoveResult.CANTMOVE;
+        }
+        if (this.pattern.getColor(x, y)) {
+            return MoveResult.CANTMOVE;
+        }
+        this.pattern.setColor(x, y, this.color);
+        if (this.pattern.check(this.color)) {
+            this.haveWin = true;
+            return MoveResult.WIN;
+        }
+        this.color = 3 - this.color;
+        return MoveResult.MOVE;
+    }
+
+    isWillWin() {
+        return this.pattern.willWin(this.color);
+    }
+}
---
+++ Week 01/TicTacToe/test/game.test.js
@@ -0,0 +1,78 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+import { GamePattern, GameControl, MoveResult } from '../src/game';
+
+describe('game test', function () {
+    it('set & check value', function () {
+        let pattern = new GamePattern;
+        for (let i = 0; i < 3; ++i) {
+            for (let j = 0; j < 3; ++j) {
+                for (let c = 0; c < 3; ++c) {
+                    pattern.setColor(i, j, c);
+                    assert.equal(pattern.getColor(i, j), c);
+                }
+            }
+        }
+    })
+    it('clone pattern', function () {
+        let pattern = new GamePattern;
+        pattern.setColor(0, 0, 2);
+        pattern.setColor(1, 1, 1);
+        let copy = pattern.clone();
+        assert.equal(copy.getColor(0, 0), pattern.getColor(0, 0));
+        assert.equal(copy.getColor(1, 1), pattern.getColor(1, 1));
+    })
+    it('check col win', function () {
+        let pattern = new GamePattern;
+        pattern.setColor(0, 1, 2);
+        pattern.setColor(1, 1, 2);
+        pattern.setColor(2, 1, 2);
+        assert.equal(pattern.check(2), true);
+    })
+    it('check row win', function () {
+        let pattern = new GamePattern;
+        pattern.setColor(2, 0, 1);
+        pattern.setColor(2, 1, 1);
+        pattern.setColor(2, 2, 1);
+        assert.equal(pattern.check(1), true);
+    })
+    it('check cross win', function () {
+        let pattern = new GamePattern;
+        pattern.setColor(0, 0, 1);
+        pattern.setColor(1, 1, 1);
+        pattern.setColor(2, 2, 1);
+        pattern.setColor(0, 2, 2);
+        pattern.setColor(2, 0, 2);
+        assert.equal(pattern.check(1), true);
+        assert.equal(pattern.check(2), false);
+        pattern.setColor(1, 1, 2);
+        assert.equal(pattern.check(1), false);
+        assert.equal(pattern.check(2), true);
+    })
+    it('check will win', function () {
+        let pattern = new GamePattern;
+        pattern.setColor(1, 0, 1);
+        pattern.setColor(1, 1, 1);
+        assert.equal(pattern.willWin(1), true);
+        assert.equal(pattern.willWin(2), false);
+        pattern.setColor(1, 2, 2);
+        pattern.setColor(2, 2, 2);
+        assert.equal(pattern.willWin(1), false);
+        assert.equal(pattern.willWin(2), true);
+    })
+    it('test round of game', function () {
+        let game = new GameControl;
+        assert.equal(game.color, 1);
+        assert.equal(game.move(1, 1), MoveResult.MOVE);
+        assert.equal(game.isWillWin(), false);
+        assert.equal(game.move(1, 1), MoveResult.CANTMOVE);
+        assert.equal(game.color, 2);
+        assert.equal(game.move(0, 0), MoveResult.MOVE);
+        assert.equal(game.move(0, 1), MoveResult.MOVE);
+        assert.equal(game.move(2, 2), MoveResult.MOVE);
+        assert.equal(game.isWillWin(), true);
+        assert.equal(game.color, 1);
+        assert.equal(game.move(2, 1), MoveResult.WIN);
+        assert.equal(game.move(0, 2), MoveResult.CANTMOVE);
+    })
+})
