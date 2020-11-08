d7aec2d5c1e032061304f6d2bb96b5ad2c6de28b
Sine Titan <565022817@qq.com>
2020-11-08 16:32:05 +0800

    reactive原理第一节

--- Week 05/Reactive/index.js
+++ Week 05/Reactive/index.js
@@ -4,11 +4,21 @@ let object = {
     a: 1, b: 2
 }
 
-let po = new Proxy(object, {
-    set(obj, prop, val) {
-        console.log(obj, prop, val);
-    }
-})
+function reactive(object) {
+    return new Proxy(object, {
+        set(obj, prop, val) {
+            obj[prop] = val;
+            console.log(obj, prop, val);
+            return obj[prop];
+        },
+        get(obj, prop) {
+            console.log(obj, prop);
+            return obj[prop];
+        }
+    })
+}
+
+let po = reactive(object);
 
 let session = repl.start();
 session.context.po = po;
