35ef5c756caf6319ae453ac98ab419931fb7185d
Sine Titan <565022817@qq.com>
2020-11-08 17:50:05 +0800

    reactive原理第三节

--- Week 05/Reactive/index.js
+++ Week 05/Reactive/index.js
@@ -1,20 +1,41 @@
-let callbacks = [];
+let callbacks = new Map();
+
+let usedReactivities = [];
 
 function effect(callback) {
-    callbacks.push(callback);
+    usedReactivities = [];
+    callback();
+
+    for (let reactivity of usedReactivities) {
+        if (!callbacks.has(reactivity[0])) {
+            callbacks.set(reactivity[0], new Map());
+        }
+        let callbackMap = callbacks.get(reactivity[0]);
+        if (!callbackMap.has(reactivity[1])) {
+            callbackMap.set(reactivity[1], new Set());
+        }
+        let callbackList = callbackMap.get(reactivity[1]);
+        callbackList.add(callback);
+    }
 } 
 
 function reactive(object) {
     return new Proxy(object, {
         set(obj, prop, val) {
             obj[prop] = val;
-            for (let callback of callbacks) {
-                callback();
+            if (callbacks.has(obj)) {
+                let callbackMap = callbacks.get(obj);
+                if (callbackMap.has(prop)) {
+                    let callbackList = callbackMap.get(prop);
+                    for (let callback of callbackList) {
+                        callback();
+                    }
+                }
             }
             return obj[prop];
         },
         get(obj, prop) {
-            console.log(obj, prop);
+            usedReactivities.push([obj, prop]);
             return obj[prop];
         }
     })
