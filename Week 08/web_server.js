82d76eab3c7ef55eb2b7a66e86a037abd61ece43
Sine Titan <565022817@qq.com>
2020-11-22 22:22:03 +0800

    简单的web服务器

---
+++ Week 08/web_server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.createServer(function (req, resp) {
+    let body = [];
+
+    req.on('error', err => console.error(err))
+        .on('data', chunk => body.push(chunk.toString()))
+        .on('end', function () {
+            let reqData = Buffer.concat(body).toString();
+            console.log("request body:", reqData);
+            body = [];
+
+            resp.writeHead(200, { 'Content-Type': 'text/html' });
+            resp.end(' Hello World\n');
+        });
+}).listen(8088);
+
+console.log("server started");
