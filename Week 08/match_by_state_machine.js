57c702c2850faac85ed3079afebfe0449af98528
Sine Titan <565022817@qq.com>
2020-11-22 18:02:23 +0800

    状态机匹配abababx

--- Week 08/match_by_state_machine.js
+++ Week 08/match_by_state_machine.js
@@ -1,19 +1,46 @@
-const start = ch => ch == 'a' ? match_a1 : start;
-const match_a1 = ch => ch == 'b' ? match_b1 : start(ch);
-const match_b1 = ch => ch == 'c' ? match_c : start(ch);
-const match_c = ch => ch == 'a' ? match_a2 : start(ch);
-const match_a2 = ch => ch == 'b' ? match_b2 : start(ch);
-const match_b2 = ch => ch == 'x' ? end : match_b1(ch);
-const end = () => end;
+const match_1 = (() => {
+    const start = ch => ch == 'a' ? match_a1 : start;
+    const match_a1 = ch => ch == 'b' ? match_b1 : start(ch);
+    const match_b1 = ch => ch == 'c' ? match_c : start(ch);
+    const match_c = ch => ch == 'a' ? match_a2 : start(ch);
+    const match_a2 = ch => ch == 'b' ? match_b2 : start(ch);
+    const match_b2 = ch => ch == 'x' ? end : match_b1(ch);
+    const end = () => end;
 
-function match(str = "", init = start, succ = end) {
-    let state = init;
-    for (let ch of str) {
-        // console.log(ch, state.name);
-        state = state(ch);
-        // console.log('->', state.name);
+    function match(str = "", init = start, succ = end) {
+        let state = init;
+        for (let ch of str) {
+            // console.log(ch, state.name);
+            state = state(ch);
+            // console.log('->', state.name);
+        }
+        return state == succ;
     }
-    return state == succ;
-}
+    return match;
+})();
 
-console.log(`"aababcabcdabcabcabcabx" has "abcabx"? ${match("aababcabcdabcabcabcabx")}`);
+console.log(`"aababcabcdabcabcabcabx" has "abcabx"? ${match_1("aababcabcdabcabcabcabx")}`);
+
+const match_2 = (() => {
+    const start = ch => ch == 'a' ? match_a1 : start;
+    const match_a1 = ch => ch == 'b' ? match_b1 : start(ch);
+    const match_b1 = ch => ch == 'a' ? match_a2 : start(ch);
+    const match_a2 = ch => ch == 'b' ? match_b2 : start(ch);
+    const match_b2 = ch => ch == 'a' ? match_a3 : match_b1(ch);
+    const match_a3 = ch => ch == 'b' ? match_b3 : match_a2(ch);
+    const match_b3 = ch => ch == 'x' ? end : match_b2(ch);
+    const end = () => end;
+
+    function match(str = "", init = start, succ = end) {
+        let state = init;
+        for (let ch of str) {
+            // console.log(ch, state.name);
+            state = state(ch);
+            // console.log('->', state.name);
+        }
+        return state == succ;
+    }
+    return match;
+})();
+
+console.log(`"aababaababababaababababababx" has "abababx"? ${match_2("aababaababababaababababababx")}`);
