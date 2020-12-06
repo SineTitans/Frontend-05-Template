dd7be2cc3409d9f920c5193adffb039789eb19fd
Sine Titan <565022817@qq.com>
2020-12-06 19:06:46 +0800

    处理html属性

--- Week 09/parser.js
+++ Week 09/parser.js
@@ -1,6 +1,7 @@
 const EOF = Symbol("end of file");
 
 let currentToken = null;
+let currentAttribute = null;
 
 function emit(token) {
     console.log(token);
@@ -75,14 +76,149 @@ function beforeAttributeName(c) {
     if (c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
     }
+    if (c == '/' || c == '>' || c == EOF) {
+        return afterAttributeName(c);
+    }
+    if (c == '=') {
+        return;
+    }
+    currentAttribute = {
+        name: "", value: "",
+    };
+    return attributeName(c);
+}
+
+function attributeName(c) {
+    if (c.match(/^[\t\n\f ]$/) || c == '/' || c == '>' || c == EOF) {
+        return afterAttributeName(c);
+    }
+    if (c == '=') {
+        return beforeAttributeValue;
+    }
+    if (c == '\u0000') {
+        return;
+    }
+    if (c == '\"' || c == "'" || c == '<') {
+        return;
+    }
+    currentAttribute.name += c;
+    return attributeName;
+}
+
+function beforeAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/) || c == EOF) {
+        return beforeAttributeValue;
+    }
+    if (c == '"') {
+        return doubleQuotedAttributeValue;
+    }
+    if (c == "'") {
+        return singleQuotedAttributeValue;
+    }
+    if (c == '>') {
+        return;
+    }
+    return UnquotedAttributeValue(c);
+}
+
+function doubleQuotedAttributeValue(c) {
+    if (c == '"') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    }
+    if (c == '\u0000') {
+        return;
+    }
+    if (c == EOF) {
+        return;
+    }
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+}
+
+function singleQuotedAttributeValue(c) {
+    if (c == "'") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    }
+    if (c == '\u0000') {
+        return;
+    }
+    if (c == EOF) {
+        return;
+    }
+    currentAttribute.value += c;
+    return singleQuotedAttributeValue;
+}
+
+function UnquotedAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return beforeAttributeName;
+    }
+    if (c == '/') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        currentToken.selfClosingStartTag = true;
+        return selfClosingStartTag;
+    }
+    if (c == '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    }
+    if (c == '\u0000') {
+        return;
+    }
+    if (c == '"' || c == "'" || c == '<' || c == '=' || c == '`') {
+        return;
+    }
+    if (c == EOF) {
+        return;
+    }
+    currentAttribute.value += c;
+    return UnquotedAttributeValue;
+}
+
+function afterQuotedAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    }
+    if (c == '/') {
+        currentToken.selfClosingStartTag = true;
+        return selfClosingStartTag;
+    }
     if (c == '>') {
         emit(currentToken);
         return data;
     }
-    if (c == '=') {
-        return beforeAttributeName;
+    if (c == EOF) {
+        return;
     }
-    return beforeAttributeName;
+    return;
+}
+
+function afterAttributeName(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        return afterAttributeName;
+    }
+    if (c == '/') {
+        currentToken.selfClosingStartTag = true;
+        return selfClosingStartTag;
+    }
+    if (c == '=') {
+        return beforeAttributeValue;
+    }
+    if (c == '>') {
+        emit(currentToken);
+        return data;
+    }
+    if (c == EOF) {
+        return;
+    }
+    currentAttribute = {
+        name: "", value: "",
+    };
+    return attributeName(c);
 }
 
 function selfClosingStartTag(c) {
