e91f1a2fc6ad5a03c40cbc2c53726abec2e967c9
Sine Titan <565022817@qq.com>
2021-01-10 23:22:17 +0800

    正确的轮播效果

--- Week 14/jsx/main.js
+++ Week 14/jsx/main.js
@@ -10,12 +10,31 @@ class Carousel extends Component {
     }
     render() {
         this.root = document.createElement("div");
+        this.root.classList.add('carousel');
         for (let record of this.attributes.src) {
-            let child = document.createElement("img");
-            child.src = record;
+            let child = document.createElement("div");
+            child.style.backgroundImage = `url(${record})`;
             this.root.appendChild(child);
         }
-        console.log(this.attributes.src);
+
+        let currentIndex = 0;
+
+        setInterval(() => {
+            let children = this.root.children;
+            let nextIndex = (currentIndex + 1) % children.length;
+            let current = children[currentIndex];
+            let next = children[nextIndex];
+
+            next.style.transition = "none";
+
+            next.style.transform = `translateX(${1 - nextIndex}00%)`;
+            setTimeout(() => {
+                next.style.transition = "";
+                current.style.transform = `translateX(${-1 - currentIndex}00%)`;
+                next.style.transform = `translateX(${-nextIndex}00%)`;
+                currentIndex = nextIndex;
+            }, 16);
+        }, 3000);
         return this.root;
     }
 }
@@ -25,7 +44,7 @@ let imgs = [
     "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
     "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
     "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
-]
+];
 
 let a = <Carousel src={imgs} />;
 
