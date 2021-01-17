4e11970fad41c801d3421d207f88bb04344c3318
Sine Titan <565022817@qq.com>
2021-01-18 01:05:10 +0800

    手势区分

--- Week 15/carousel/gesture.js
+++ Week 15/carousel/gesture.js
@@ -40,18 +40,54 @@ element.addEventListener("touchcancel", event => {
     }
 });
 
+let handler;
+let startX, startY;
+let isTap = true, isPan = false, isPress = false;
+
 let start = point => {
-    console.log("start", point.clientX, point.clientY);
+    startX = point.clientX, startY = point.clientY;
+
+    isTap = true, isPan = false, isPress = false;
+
+    handler = setTimeout(() => {
+        isTap = false, isPan = false, isPress = true;
+        handler = null;
+        console.log("press");
+    }, 500);
 };
 
 let move = point => {
-    console.log("move", point.clientX, point.clientY);
+    let dx = point.clientX - startX, dy = point.clientY - startY;
+    if (!isPan && dx * dx + dy * dy > 100) {
+        isTap = false, isPan = true, isPress = false;
+
+        console.log("pan start");
+        clearTimeout(handler);
+    }
+
+    if (isPan) {
+        console.log(dx, dy);
+        console.log("pan");
+    }
+
+    // console.log("move", point.clientX, point.clientY);
 };
 
 let end = point => {
-    console.log("end", point.clientX, point.clientY);
+    if (isTap) {
+        console.log("tap");
+        clearTimeout(handler);
+    }
+    if (isPan) {
+        console.log("pan end");
+    }
+    if (isPress) {
+        console.log("press end");
+    }
+    // console.log("end", point.clientX, point.clientY);
 };
 
 let cancel = point => {
+    clearTimeout(handler);
     console.log("cancel", point.clientX, point.clientY);
 };
