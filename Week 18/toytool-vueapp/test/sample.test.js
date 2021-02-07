a0a812302f2fd4a78aa74590a1c41f93823f7967
Sine Titan <565022817@qq.com>
2021-02-08 00:10:37 +0800

    测试新版本的项目脚手架

---
+++ Week 18/toytool-vueapp/test/sample.test.js
@@ -0,0 +1,8 @@
+import assert from "assert";
+import { describe, it } from "mocha";
+
+describe("test cases", function () {
+    it("1 + 1 == 2", function () {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
