f6bcf79e3b09fdf348e37d4ce419e5efccd4452e
Sine Titan <565022817@qq.com>
2021-02-17 19:10:40 +0800

    获取文件大小

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -2,6 +2,7 @@ let http = require('http');
 let fs = require('fs');
 
 http.createServer(function(req, resp) {
+    console.log("req headers:", req.headers);
     let outFile = fs.createWriteStream("../server/public/index.html");
     req.pipe(outFile);
     req.on('end', () => resp.end("Success"));
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,15 +1,19 @@
 let http = require('http');
 let fs = require('fs');
 
-let request = http.request({
-    hostname: "172.30.167.94",
-    port: 8082,
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/octet-stream',
-    }
-}, resp => resp.pipe(process.stdout));
+fs.stat("./sample.html", function (err, stats) {
 
-let file = fs.createReadStream("./sample.html");
-file.pipe(request);
-file.on('end', () => request.end());
+    let request = http.request({
+        hostname: "172.30.167.94",
+        port: 8082,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Content-Length': stats.size,
+        }
+    }, resp => resp.pipe(process.stdout));
+    
+    let file = fs.createReadStream("./sample.html");
+    file.pipe(request);
+    file.on('end', () => request.end());
+});
