c9056935113735e0aa4f12b9c9062c2e5b4edfcc
Sine Titan <565022817@qq.com>
2021-02-17 20:43:49 +0800

    压缩发布整个文件夹

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,9 +1,9 @@
 let http = require('http');
-let fs = require('fs');
+let unzipper = require('unzipper');
 
 http.createServer(function(req, resp) {
     console.log("req headers:", req.headers);
-    let outFile = fs.createWriteStream("../server/public/index.html");
-    req.pipe(outFile);
+
+    req.pipe(unzipper.Extract({ path: "../server/public/" }));
     req.on('end', () => resp.end("Success"));
 }).listen(8082);
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,19 +1,21 @@
 let http = require('http');
-let fs = require('fs');
+let archiver = require('archiver');
 
-fs.stat("./sample.html", function (err, stats) {
-
-    let request = http.request({
-        hostname: "172.30.167.94",
-        port: 8082,
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/octet-stream',
-            'Content-Length': stats.size,
-        }
-    }, resp => resp.pipe(process.stdout));
-    
-    let file = fs.createReadStream("./sample.html");
-    file.pipe(request);
-    file.on('end', () => request.end());
+const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
 });
+
+archive.directory('./sample/', false);
+archive.finalize();
+
+let request = http.request({
+    hostname: "172.30.167.94",
+    // hostname: "127.0.0.1",
+    port: 8082,
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/octet-stream',
+    }
+}, resp => resp.pipe(process.stdout));
+
+archive.pipe(request);
